35916863e8b7639ecc7ef27da934fa9ae220b5b2
Jose Carlos Pereira <zecam64800@gmail.com>
2023-12-04 01:22:28 +0000

    Final Jenkins ec2 terraform

--- infrastructure/terraform/ec2.tf
+++ infrastructure/terraform/ec2.tf
@@ -29,27 +29,46 @@ resource "aws_instance" "myec2" {
   provisioner "remote-exec" {
     inline = [
       //install jenkins
-      "sudo sudo yum update –y",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
+      "sudo sudo yum update",
       "sudo yum upgrade",
+      //install jdk
       "sudo dnf install java-17-amazon-corretto -y",
-      "sudo yum install jenkins -y",
-      "sudo systemctl enable jenkins",
-      "sudo systemctl start jenkins",
-      "sudo systemctl status jenkins --no-pager",
+      //install docker
+      "sudo yum search docker",
+      "sudo yum install docker -y",
+      "sudo usermod -a -G docker ec2-user",
+      "sudo systemctl enable docker.service",
+      "sudo systemctl start docker.service",
+      //install mvn
+      "wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.tar.gz -P /tmp",
+      "sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt",
+      "sudo ln -s /opt/apache-maven-3.9.5 /opt/maven",
+      //set maven for services
+      "echo 'DefaultEnvironment=\"M2_HOME=/opt/maven\"'| sudo tee --append /etc/systemd/system.conf",
+      "echo 'DefaultEnvironment=\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/maven/bin\"'| sudo tee --append /etc/systemd/system.conf",
+      "sudo systemctl daemon-reexec",
+      //set maven on global environment
+      "echo 'M2_HOME=\"/opt/maven\"'| sudo tee --append /etc/environment",
+      "echo 'PATH=\"/opt/maven/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games\"'| sudo tee --append /etc/environment",
       //install git
       "sudo dnf install git-all -y",
+      //create permanent swap file to add more virtual memory to t2.micro
       "sudo dd if=/dev/zero of=/swapfile bs=128M count=32",
       "sudo chmod 600 /swapfile",
       "sudo mkswap /swapfile",
       "sudo swapon /swapfile",
       "sudo chmod 777 /etc/fstab",
-      "udo echo '/swapfile swap swap defaults 0 0' >> /etc/fstab",
-      //install mvn
-      "wget https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.tar.gz -P /tmp",
-      "sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt",
-      "sudo ln -s /opt/apache-maven-3.9.5 /opt/maven"
+      "sudo echo '/swapfile swap swap defaults 0 0' >> /etc/fstab",
+      //install jenkins,sudo systemctl status jenkins --no-pager on ec2 to get admin pass
+      // systemctl edit jenkins to set for jenkins only
+      // go Dashboard>Manage Jenkins>System and remove github api rate limit
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
+      "sudo yum install jenkins -y",
+      "sudo systemctl enable jenkins",
+      "sudo usermod -a -G docker jenkins",
+      "sudo systemctl start jenkins",
+      "sudo systemctl status jenkins --no-pager"
     ]
   }
 
@@ -99,3 +118,8 @@ data "aws_secretsmanager_secret_version" "creds" {
 locals {
   db_creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
 }*/
+
+//"sudo chmod 777 /etc/environment",
+//"sudo echo 'M2_HOME=\"/opt/maven\"' >> /etc/environment",
+//"sudo echo 'PATH=\"/opt/maven/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games\"' >> /etc/environment",
+//"sudo chmod 744 /etc/environment",
