dbaeefb1e3d6a44076fc6c6243b2572f870ee2c1
Jose Carlos Pereira <zecam64800@gmail.com>
2023-12-04 05:26:29 +0000

    Final Jenkins ec2 terraform

--- infrastructure/terraform/ec2.tf
+++ infrastructure/terraform/ec2.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "myec2" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo sudo yum update –y",
+      "sudo sudo yum update –y ",
       "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
       "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
       "sudo yum upgrade",
